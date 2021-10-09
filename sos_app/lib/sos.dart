0d7e0e8cb0ea9d40830cb75071d629d5d63be0f8
Rabaa-basha <55857464+Rabaa-basha@users.noreply.github.com>
2021-10-09 02:49:52 +0200

    Update sos.dart

--- sos_app/lib/sos.dart
+++ sos_app/lib/sos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 
 class SOS extends StatefulWidget {
   @override
@@ -7,54 +8,64 @@ class SOS extends StatefulWidget {
 
 class SOS_State extends State<SOS> {
 
-int _currentIndex = 1;
+  int _currentIndex = 1;
 
-   final tabs = [
-     Center(child: Text('Activities')),
-     Center(child: Text('SOS')),
-     Center(child: Text('Profile')),
-   ];
+  final tabs = [
+    Center(child: Text('Activities')),
+    Center(child: Text('SOS')),
+    Center(child: Text('Profile')),
+  ];
+
+  void _callNumber() async{
+    const number = '01154703796'; //set the number here
+    bool? res = await FlutterPhoneDirectCaller.callNumber(number);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('SOS '),
-        centerTitle: true,
-      ),
+        appBar: AppBar(
+          title: Text('SOS '),
+          centerTitle: true,
+        ),
 
-      body: tabs[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
+        body: tabs[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
 
-        currentIndex: _currentIndex,
+          currentIndex: _currentIndex,
 
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: 'Activities',
-            backgroundColor: Colors.blue
-          ),
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications),
+                label: 'Activities',
+                backgroundColor: Colors.blue
+            ),
 
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.blue
-          ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+                backgroundColor: Colors.blue
+            ),
 
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-            backgroundColor: Colors.blue
-          )
-        ],
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+                backgroundColor: Colors.blue
+            )
+          ],
 
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        }
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          }
 
-      ,)
+          ,),
+      floatingActionButton: FloatingActionButton(
+      onPressed: _callNumber,
+      tooltip: 'Increment',
+      child: const Icon(Icons.call_rounded),
+    ),
     );
   }
 }
