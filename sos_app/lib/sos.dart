3e64e038486291a975ec5e5254ca2b6809d733f4
ZneR <zner26@gmail.com>
2021-10-14 11:39:31 -0600

    Upgrading Flutter to 2.5.2 from 2.0.3
    - Need to upgrade to allow use of new packages such as the default flutter camera package
    @everyone flutter upgrade
    ^ You may have to force this command via adding --force
    Just be careful and have backups

--- sos_app/lib/sos.dart
+++ sos_app/lib/sos.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
-import 'package:flutter/services.dart';
+import 'package:camera/camera.dart';
 import 'dart:async';
 import 'profile.dart';
 
