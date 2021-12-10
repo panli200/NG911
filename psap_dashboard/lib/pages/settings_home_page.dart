96fe9143f69398966434deb174d5e92a9e0ae182
ZneR <zner26@gmail.com>
2021-12-10 07:17:09 -0600

    Complete

--- psap_dashboard/lib/pages/settings_home_page.dart
+++ psap_dashboard/lib/pages/settings_home_page.dart
@@ -4,9 +4,63 @@ class SettingsHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
-        title: Text('Settings'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-      ),
-    );
+      title: Text('Reset Password'),
+      centerTitle: true,
+      backgroundColor: Colors.green,
+    ),
+
+    body: 
+      Container(
+        child: 
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              Center(
+                child: 
+                  Text('Username  admin'),
+              ),
+
+              SizedBox(height: 20), // Spacing visuals
+
+              Container(
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: 
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'New Password',
+                    )
+                  ),
+              ),
+
+              SizedBox(height: 20), // Spacing visuals
+
+              Container(
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: 
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Confirm Password',
+                    )
+                  ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: 
+                  ElevatedButton(
+                    onPressed: () {
+
+                    },
+                    child: const Text('Submit'),
+                  )
+              )
+            ],
+          ),
+      )
+  );
 }
