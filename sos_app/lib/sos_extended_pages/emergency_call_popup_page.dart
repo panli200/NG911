fdaea016b665b8f1f96cc19196d62a5abe0cedee
Rabaa-basha <beborabaa@gmail.com>
2021-12-21 15:18:25 +0200

    JSON-ing sensor data to realtime database from sosApp

--- sos_app/lib/sos_extended_pages/emergency_call_popup_page.dart
+++ sos_app/lib/sos_extended_pages/emergency_call_popup_page.dart
@@ -3,6 +3,15 @@ import 'package:auto_route/auto_route.dart';
 import 'package:sos_app/data/application_data.dart';
 import 'package:sos_app/routes/router.gr.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:sos_app/services/location.dart';
+import 'package:sos_app/services/weather.dart';
+import 'package:sos_app/services/acceleration.dart';
+import 'package:sensors/sensors.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:sos_app/services/location.dart';
+import 'package:battery/battery.dart';
+import 'dart:math';
 
 class EmergencyCallPopUpPage extends StatelessWidget {
 
@@ -14,9 +23,67 @@ class EmergencyCallPopUpPage extends StatelessWidget {
   }) : super(key: key);
 
   void _callNumber() async{
+    _updateSensors();
+
+
+
     const number = '01154703796'; //set the number here
     bool? res = await FlutterPhoneDirectCaller.callNumber(number);
+
   }
+
+  void _updateSensors(){
+    Location location = Location();
+    location.getCurrentLocation();
+
+    var _battery = Battery();
+    final FirebaseDatabase database = FirebaseDatabase.instance;
+    DatabaseReference ref = FirebaseDatabase.instance.ref();
+    final databaseReal = ref.child('sensors').child('kgYjwJEbFNPP8JvD1PM8vcgxMxh2');
+
+    // For display, can be delete in the future
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
+    String Longitude = location.longitude.toString();
+    String Latitude = location.latitude.toString();
+    bool Danger = false;
+    accelerometerEvents.listen((AccelerometerEvent event) {
+      x = event.x;
+      y = event.y;
+      z = event.z;
+      double AccelerationMagnitude = sqrt(
+          pow(x, 2) + pow(y, 2) + pow(z, 2));
+      if (AccelerationMagnitude > 10.0) {
+        Danger = true;
+      } else {
+        Danger = false;
+      }
+      String batLevel= _battery.batteryLevel.toString();
+      databaseReal.update(
+          {
+            'Latitude': location.latitude.toString(),
+            'Longitude': location.longitude.toString(),
+            'x-Acc': x,
+            'y-Acc': y,
+            'z-Acc': z,
+            'MobileCharge': batLevel,
+          }
+      );
+
+
+    });
+    _battery.onBatteryStateChanged.listen((BatteryState state) {
+      databaseReal.update(
+          {
+            'MobileCharge': _battery.batteryLevel.toString(),
+
+          }
+      );
+    });
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -85,7 +152,7 @@ class EmergencyCallPopUpPage extends StatelessWidget {
                   //color: Colors.grey,
                       child:ElevatedButton(
                           child: Text("Third Party (Bystander)"),
-                          onPressed: _callNumber // this will the method for your rejected Button
+                          onPressed: _callNumber,  // this will the method for your rejected Button
                       )
                 ), 
               ), 
