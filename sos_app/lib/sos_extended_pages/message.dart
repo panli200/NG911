c40af0d77b21f46bf57ed40d8228558966c9aaf4
Li Pan <panli200@uregina.ca>
2021-12-31 11:05:36 -0600

    enable the message function on sos

--- sos_app/lib/sos_extended_pages/message.dart
+++ sos_app/lib/sos_extended_pages/message.dart
@@ -12,15 +12,9 @@ class Message extends StatefulWidget {
 }
 
 class _Message extends State<Message> {
-
-  bool _isLogin = true;
-
   final _peerMessageController = TextEditingController();
-
   final _infoStrings = <String>[];
-
   late AgoraRtmClient _client;
-
   String userId = FirebaseAuth.instance.currentUser!.uid;
 
   @override
@@ -36,12 +30,9 @@ class _Message extends State<Message> {
       child: Column(
         children: [
           //_buildLogin(),
-          SizedBox(
-            height: 8.0,
-          ),
           _buildInfoList(),
           SizedBox(
-            height: 8.0,
+            height: 16.0,
           ),
           _buildSendPeerMessage(),
         ],
@@ -51,45 +42,13 @@ class _Message extends State<Message> {
 
   void _createClient() async {
     _client = await AgoraRtmClient.createInstance(APP_ID);
+    _client.login(null, userId); //create client and also login
     _client?.onMessageReceived = (AgoraRtmMessage message, String peerId) {
-      _log("Peer msg: " + peerId + ", msg: " + (message.text));
-    };
-    _client?.onConnectionStateChanged = (int state, int reason) {
-      _log('Connection state changed: ' +
-          state.toString() +
-          ', reason: ' +
-          reason.toString());
-      _client.login(null, userId); //create client and also login
-      if (state == 5) {
-        _client?.logout();
-        _log('Logout.');
-        setState(() {
-          _isLogin = false;
-        });
-      }
+      _log(peerId + " : " + (message.text));
     };
   }
 
-  // Widget _buildLogin() {
-  //   return new ElevatedButton(
-  //     child: Text(
-  //       'Close',
-  //     ),
-  //     style: ElevatedButton.styleFrom(
-  //       primary: Colors.red, // background (button) color
-  //       onPrimary: Colors.white, // foreground (text) color
-  //     ),
-  //     onPressed: () {
-  //       _isLogin = false;
-  //       Navigator.pop(context);
-  //     },
-  //   );
-  // }
-
   Widget _buildSendPeerMessage() {
-    if (!_isLogin) {
-      return Container();
-    }
     return Row(children: <Widget>[
       new Expanded(
           child: new TextField(
@@ -106,43 +65,17 @@ class _Message extends State<Message> {
     return Expanded(
         child: Container(
             child: ListView.builder(
-              itemExtent: 24,
-              itemBuilder: (context, i) {
-              return ListTile(
-                contentPadding: const EdgeInsets.all(0.0),
-                title: Text(_infoStrings[i]),
-              );
-            },
+      reverse: true,
+      itemExtent: 20,
+      itemBuilder: (context, i) {
+        return ListTile(
+          contentPadding: const EdgeInsets.all(0.0),
+          title: Text(_infoStrings[i]),
+        );
+      },
       itemCount: _infoStrings.length,
     )));
   }
-
-  // void _toggleLogin() async {
-  //   if (_isLogin) {
-  //     try {
-  //       await _client?.logout();
-  //       _log('Disconnect success.');
-  //
-  //       setState(() {
-  //         _isLogin = false;
-  //       });
-  //     } catch (errorCode) {
-  //       _log('Disconnect error: ' + errorCode.toString());
-  //     }
-  //   } else {
-  //     String userId = FirebaseAuth.instance.currentUser!.uid;
-  //
-  //     try {
-  //       await _client?.login(null, userId);
-  //       _log('Connect success: ' + userId);
-  //       setState(() {
-  //         _isLogin = true;
-  //       });
-  //     } catch (errorCode) {
-  //       _log('Connect error: ' + errorCode.toString());
-  //     }
-  //   // }
-  // }
 
   void _toggleSendPeerMessage() async {
     String peerUid = '911'; //fixed peerUid 911, will fix it in firebase
@@ -159,7 +92,7 @@ class _Message extends State<Message> {
       await _client?.sendMessageToPeer(peerUid, message, false);
       _log('Send message to 911 success.');
     } catch (errorCode) {
-      _log('Send message fails! error: ' + errorCode.toString());
+      _log('Send message fails!');
     }
   }
 
