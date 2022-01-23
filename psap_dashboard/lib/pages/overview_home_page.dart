838c5f5def45719c7f1ddcb91505737ce674bd80
Li Pan <panli200@uregina.ca>
2022-01-23 14:13:02 -0600

    chart dynamic

--- psap_dashboard/lib/pages/overview_home_page.dart
+++ psap_dashboard/lib/pages/overview_home_page.dart
@@ -20,35 +20,12 @@ class _OverviewHomePageState extends State<OverviewHomePage> {
 
   List<int> num = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-  void getNum() {
-    FirebaseFirestore.instance
-        .collection('History')
-        .get()
-        .then((QuerySnapshot querySnapshot)  async {
-        for (var doc in querySnapshot.docs) {
-          if (DateTime.parse(doc.id).year == currentDay.year &&
-              DateTime.parse(doc.id).month == currentDay.month &&
-              DateTime.parse(doc.id).day == currentDay.day) {
-            for (int i = 0; i < 24; i++) {
-              if (DateTime.parse(doc.id).hour == i) {
-                  setState(() {
-                    num[i] = num[i] + 1;
-                });
-              }
-
-            }
-          }
-        }
-
-    });
-
-  }
 
   @override
   void initState() {
     // Timer.periodic(Duration(seconds: 1), (t) {  });
 
-
+    //getNum();
     super.initState();
   }
 
@@ -70,43 +47,74 @@ class _OverviewHomePageState extends State<OverviewHomePage> {
                 SizedBox(
                     height: MediaQuery.of(context).size.height * 0.9,
                     width: MediaQuery.of(context).size.width * 0.6,
-                    child: SfCartesianChart(
-                        // Initialize category axis
-                        primaryXAxis: CategoryAxis(),
-                        series: <LineSeries<Emergency, String>>[
-                          LineSeries<Emergency, String>(
-                              // Bind data source
-                              dataSource: <Emergency>[
-                                Emergency('01:00', num[0]),
-                                Emergency('02:00', num[1]),
-                                Emergency('03:00', num[2]),
-                                Emergency('04:00', num[3]),
-                                Emergency('05:00', num[4]),
-                                Emergency('06:00', num[5]),
-                                Emergency('07:00', num[6]),
-                                Emergency('08:00', num[7]),
-                                Emergency('09:00', num[8]),
-                                Emergency('10:00', num[9]),
-                                Emergency('11:00', num[10]),
-                                Emergency('12:00', num[11]),
-                                Emergency('13:00', num[12]),
-                                Emergency('14:00', num[13]),
-                                Emergency('15:00', num[14]),
-                                Emergency('16:00', num[15]),
-                                Emergency('17:00', num[16]),
-                                Emergency('18:00', num[17]),
-                                Emergency('19:00', num[18]),
-                                Emergency('20:00', num[19]),
-                                Emergency('21:00', num[20]),
-                                Emergency('22:00', num[21]),
-                                Emergency('23:00', num[22]),
-                                Emergency('24:00', num[23]),
-                              ],
-                              xValueMapper: (Emergency amount, _) =>
-                                  amount.hour,
-                              yValueMapper: (Emergency amount, _) =>
-                                  amount.amount)
-                        ])),
+
+                    child:StreamBuilder<QuerySnapshot>(
+                        stream: history,
+                        builder: (
+                            BuildContext context,
+                            AsyncSnapshot<QuerySnapshot> snapshot,
+                            ) {
+                          if (snapshot.hasError) {
+                            return Text(
+                                'Something went wrong  ${snapshot.error}');
+                          }
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Text('Loading');
+                          }
+                          final data = snapshot.requireData;
+                          for (var doc in data.docs) {
+                            if (DateTime.parse(doc.id).year == currentDay.year &&
+                                DateTime.parse(doc.id).month == currentDay.month &&
+                                DateTime.parse(doc.id).day == currentDay.day) {
+                              for (int i = 0; i < 24; i++) {
+                                if (DateTime.parse(doc.id).hour == i) {
+                                    num[i] = num[i] + 1;
+                                }
+                              }
+                            }
+                          }
+                          return   SfCartesianChart(
+                           // Initialize category axis
+                              primaryXAxis: CategoryAxis(),
+                              series: <LineSeries<Emergency, String>>[
+                                LineSeries<Emergency, String>(
+                                    // Bind data source
+                                    dataSource: <Emergency>[
+                                      Emergency('01:00', num[0]),
+                                      Emergency('02:00', num[1]),
+                                      Emergency('03:00', num[2]),
+                                      Emergency('04:00', num[3]),
+                                      Emergency('05:00', num[4]),
+                                      Emergency('06:00', num[5]),
+                                      Emergency('07:00', num[6]),
+                                      Emergency('08:00', num[7]),
+                                      Emergency('09:00', num[8]),
+                                      Emergency('10:00', num[9]),
+                                      Emergency('11:00', num[10]),
+                                      Emergency('12:00', num[11]),
+                                      Emergency('13:00', num[12]),
+                                      Emergency('14:00', num[13]),
+                                      Emergency('15:00', num[14]),
+                                      Emergency('16:00', num[15]),
+                                      Emergency('17:00', num[16]),
+                                      Emergency('18:00', num[17]),
+                                      Emergency('19:00', num[18]),
+                                      Emergency('20:00', num[19]),
+                                      Emergency('21:00', num[20]),
+                                      Emergency('22:00', num[21]),
+                                      Emergency('23:00', num[22]),
+                                      Emergency('24:00', num[23]),
+                                    ],
+                                    xValueMapper: (Emergency amount, _) =>
+                                        amount.hour,
+                                    yValueMapper: (Emergency amount, _) =>
+                                        amount.amount)
+                              ]);
+
+                        }),
+
+                ),
                 SizedBox(
                     height: MediaQuery.of(context).size.height * 0.9,
                     width: MediaQuery.of(context).size.width * 0.4,
