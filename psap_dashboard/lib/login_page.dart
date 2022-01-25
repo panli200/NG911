5421e218aad65e42f7dfdab266380a73a4307f36
Li Pan <panli200@uregina.ca>
2022-01-24 20:30:49 -0600

    login display

---
+++ psap_dashboard/lib/login_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:psap_dashboard/pages/maps_home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    // drawer: NavigationDrawerWidget(),
+    // appBar: AppBar(
+    //   title: Text(MyApp.title),
+    // ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Center(
+            child: Text(
+              'Saskatchewan Public Safety Agency 911',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+              textAlign: TextAlign.center,
+            ),
+          ),
+
+          const SizedBox(height: 80), // Spacing visuals
+
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.3,
+            child: const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                )),
+          ),
+          const SizedBox(height: 20), // Spacing visuals
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.3,
+            child: const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                )),
+          ),
+          const SizedBox(height: 40),
+          SizedBox(
+              width: MediaQuery.of(context).size.width * 0.3,
+              height: MediaQuery.of(context).size.width * 0.03,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const MapsHomePage()));
+                },
+                child: const Text('LOGIN'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red, // background
+                ),
+              ))
+        ],
+      ));
+}
--- psap_dashboard/lib/main.dart
+++ psap_dashboard/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:psap_dashboard/widget/navigation_drawer_widget.dart';
+import 'package:psap_dashboard/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future main() async {
@@ -14,22 +14,13 @@ Future main() async {
     // Replace with actual values
     options: const FirebaseOptions(
         apiKey: "AIzaSyB5Z8MWrQ5ZZJCUJBZhL6r_Axf_OiZGdEc",
-
         authDomain: "sos-app-3af53.firebaseapp.com",
-
         databaseURL: "https://sos-app-3af53-default-rtdb.firebaseio.com",
-
         projectId: "sos-app-3af53",
-
         storageBucket: "sos-app-3af53.appspot.com",
-
         messagingSenderId: "728757332541",
-
         appId: "1:728757332541:web:22b0f46cb91234e88e5854",
-
-        measurementId: "G-GH5H3WTRBN"
-
-    ),
+        measurementId: "G-GH5H3WTRBN"),
   );
   runApp(MyApp());
 }
@@ -38,24 +29,10 @@ class MyApp extends StatelessWidget {
   static final String title = 'PSAP Control Panel';
   @override
   Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: title,
-    theme: ThemeData(primarySwatch: Colors.blue),
-    home: MainPage(),
-  );
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: LoginPage(),
+      );
 }
 
-class MainPage extends StatefulWidget {
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  @override
-  Widget build(BuildContext context) => Scaffold(
-    drawer: NavigationDrawerWidget(),
-    appBar: AppBar(
-      title: Text(MyApp.title),
-    ),
-  );
-}
