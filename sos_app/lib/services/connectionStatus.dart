a05cb867c5a16099ced2d7b4f8ecce97a989436d
Rabaa-basha <beborabaa@gmail.com>
2022-02-22 20:14:57 -0600

    trying to stream connection status

--- sos_app/lib/services/connectionStatus.dart
+++ sos_app/lib/services/connectionStatus.dart
@@ -1,11 +1,47 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
 
-Future<bool> isConnected() async{
-  var connectivityResult = await (Connectivity().checkConnectivity());
-  if (connectivityResult == ConnectivityResult.none) {
-      return false;
-  } else{
-    return true;
+class ConnectionStatus extends StatefulWidget{
+  ConnectionStatus(
+      {Key? key}): super(key: key);
+
+  @override
+  _ConnectionStatusState createState() => _ConnectionStatusState();
+}
+
+
+class _ConnectionStatusState extends State<ConnectionStatus>{
+  bool connected = false;
+  String connection = '';
+  StreamSubscription? subscriptionConnection;
+
+  @override
+  void initState(){
+     subscriptionConnection = Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
+      if(result == ConnectivityResult.none) {
+        setState(() {
+          connection = 'No internet connection';
+        });
+      }else{
+        setState(() {
+          connection = 'Connected';
+        });
+      }
+    });
+    super.initState();
   }
+
+  @override
+  void dispose(){
+    subscriptionConnection!.cancel();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Text(connection);
+  }
+
 
 }
