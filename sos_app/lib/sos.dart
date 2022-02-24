cfbd7a1750d67528a44b3251b68304fc02ddea93
Li Pan <panli200@uregina.ca>
2022-02-23 20:23:31 -0600

    delete the old code

--- sos_app/lib/sos.dart
+++ sos_app/lib/sos.dart
@@ -4,52 +4,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_remix/flutter_remix.dart';
 
 class SosPage extends StatelessWidget {
-
   const SosPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AutoTabsScaffold(
-      // appBarBuilder: (_, tabsRouter) => AppBar(
-      //   title: Text(tabsRouter.current.name),
-      //   centerTitle: true,
-      //   leading: IconButton(
-      //     icon: Icon(Icons.chevron_left),
-      //     onPressed: () => Navigator.pop(context, false),
-      //   ),
-      //
-      // ),
-      routes: const [
-        SOS(),
-        Profile(),
-        Activities(),
-      ],
-
-      bottomNavigationBuilder: (_, tabsRouter) {
-        return BottomNavigationBar(
-
-          currentIndex: tabsRouter.activeIndex,
-          onTap: tabsRouter.setActiveIndex,
-
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(FlutterRemix.alarm_warning_line),
-              label: 'SOS',
-              backgroundColor: Colors.blue
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(FlutterRemix.profile_line),
-              label: 'Profile',
-              backgroundColor: Colors.blue
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(FlutterRemix.folder_history_line),
-              label: 'Logs',
-              backgroundColor: Colors.blue
-            ),
-          ],
-        );
-      }
-    );
+        routes: const [
+          SOS(),
+          Profile(),
+          Activities(),
+        ],
+        bottomNavigationBuilder: (_, tabsRouter) {
+          return BottomNavigationBar(
+            currentIndex: tabsRouter.activeIndex,
+            onTap: tabsRouter.setActiveIndex,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(FlutterRemix.alarm_warning_line),
+                  label: 'SOS',
+                  backgroundColor: Colors.blue),
+              BottomNavigationBarItem(
+                  icon: Icon(FlutterRemix.profile_line),
+                  label: 'Profile',
+                  backgroundColor: Colors.blue),
+              BottomNavigationBarItem(
+                  icon: Icon(FlutterRemix.folder_history_line),
+                  label: 'Logs',
+                  backgroundColor: Colors.blue),
+            ],
+          );
+        });
   }
 }
