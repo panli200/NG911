142c84f7ada403534abd8542444521843382cf43
Li Pan <panli200@uregina.ca>
2022-03-13 15:18:37 -0600

    multiple lines display
    using multiple lines to solve the overflow on right side

--- psap_dashboard/lib/pages/health__info_buttons.dart
+++ psap_dashboard/lib/pages/health__info_buttons.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:url_launcher/url_launcher.dart';
+
 class CaseBasic extends StatelessWidget {
-   int type;
+  int type;
   final phone;
   final String emergencyContactNumberString;
   final String emergencyHealthCardNumberString;
   final String personalHealthCardString;
   final String urlPMR; // url of personal medical report
   final String urlECMR; // url of emergency contact medical report
-  CaseBasic({Key? key, required this.type, required this.phone, required this.emergencyContactNumberString, required this.emergencyHealthCardNumberString, required this.personalHealthCardString, required this.urlPMR, required this.urlECMR}): super(key: key);
-
-
-
-
-
+  CaseBasic(
+      {Key? key,
+      required this.type,
+      required this.phone,
+      required this.emergencyContactNumberString,
+      required this.emergencyHealthCardNumberString,
+      required this.personalHealthCardString,
+      required this.urlPMR,
+      required this.urlECMR})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if(type == 1 && emergencyContactNumberString == ""){
+    if (type == 1 && emergencyContactNumberString == "") {
       type = 5;
-    }else if(type == 2 && ((emergencyContactNumberString == "") || (personalHealthCardString == ""))){
+    } else if (type == 2 &&
+        ((emergencyContactNumberString == "") ||
+            (personalHealthCardString == ""))) {
       type = 5;
-    }else if(type == 3 && personalHealthCardString == ""){
+    } else if (type == 3 && personalHealthCardString == "") {
       type = 5;
-    }else if(type == 4 && emergencyHealthCardNumberString == ""){
+    } else if (type == 4 && emergencyHealthCardNumberString == "") {
       type = 5;
     }
     void downloadTheURL() async {
@@ -34,7 +41,7 @@ class CaseBasic extends StatelessWidget {
         else
           // can't launch url, there is some error
           throw "Could not launch $urlPMR";
-      }else{
+      } else {
         if (await canLaunch(urlECMR))
           await launch(urlECMR);
         else
@@ -42,20 +49,22 @@ class CaseBasic extends StatelessWidget {
           throw "Could not launch $urlECMR";
       }
     }
-    switch(type){
+
+    switch (type) {
       case 1: // Personal call, only first permission is granted
-        return Column(
-            children: [
-          Text("Emergency contact number: " + emergencyContactNumberString),
+        return Column(children: [
+          const Text("Emergency contact number: "),
+          Text(emergencyContactNumberString),
         ]);
 
       case 2: // Personal call, two permissions are granted
 
         return Column(children: [
-          Text("Emergency contact number: " + emergencyContactNumberString),
+          const Text("Emergency contact number: "),
+          Text(emergencyContactNumberString),
           Text("Personal health card: " + personalHealthCardString),
           ElevatedButton(
-              onPressed:downloadTheURL,
+              onPressed: downloadTheURL,
               child: const Text("Download Personal Medical Report")),
         ]);
 
@@ -69,20 +78,16 @@ class CaseBasic extends StatelessWidget {
 
       case 4: // Emergency contact call, with Permission granted
         return Column(children: [
-          Text("Emergency contact health card number: " + emergencyHealthCardNumberString),
+          const Text("Emergency contact health card number: "),
+          Text(emergencyHealthCardNumberString),
           ElevatedButton(
               onPressed: downloadTheURL,
               child: const Text("Download Emergency contact Medical Report")),
         ]);
 
       default: // Type 0 expected
-        return Column(children: const [
-
-        ]);
-
+        return Column(children: const []);
     }
-
-
   }
 
   @override
@@ -91,4 +96,3 @@ class CaseBasic extends StatelessWidget {
     throw UnimplementedError();
   }
 }
-
